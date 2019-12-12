f508667dab382b1a163b37157b289b3e886d8c75
John Kakuk <41341962+johnkakuk@users.noreply.github.com>
2019-12-12 10:19:31 -0400

    File structure reorganization

---
+++ Art.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ASCII_to_Code
+{
+    class Art
+    {
+        public static void Create(Settings Config)
+        {
+            // Setup
+            List<string> UserInput = new List<string>();
+
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine(@"################################################################################");
+                Console.WriteLine(@"                _   ___  ___ ___ ___   _          ___         _                 ");
+                Console.WriteLine(@"               /_\ / __|/ __|_ _|_ _| | |_ ___   / __|___  __| |___             ");
+                Console.WriteLine(@"              / _ \\__ \ (__ | | | |  |  _/ _ \ | (__/ _ \/ _` / -_)            ");
+                Console.WriteLine(@"             /_/ \_\___/\___|___|___|  \__\___/  \___\___/\__,_\___|            ");
+                Console.WriteLine(@"                                                                                ");
+                Console.WriteLine(@"################################################################################");
+                Console.WriteLine("Paste your ASCII art. Type \"done\" on a newline when finished.");
+                Console.WriteLine("Type \"clear\" on a newline to start over.");
+                Console.WriteLine("Type \"exit\" on a newline to return to the main menu.");
+
+                UserInput = Capture();
+
+                if (UserInput[0] == "exit")
+                {
+                    return;
+                } else if (UserInput[0] == "clear")
+                {
+                    UserInput.Clear();
+                    continue;
+                }
+
+                Operate(Config, UserInput);
+
+            }
+        }
+
+
+
+
+        public static List<string> Capture()
+        {
+            // Capture user input, line-by-line, until the user types an escape statement on a new line
+            string currentLine = Console.ReadLine();
+            List<string> UserInput = new List<string>();
+
+            while (currentLine.ToLower() != "clear" && currentLine.ToLower() != "done" && currentLine.ToLower() != "exit")
+            {
+                UserInput.Add(currentLine);
+
+                currentLine = Console.ReadLine();
+            }
+
+            switch (currentLine)
+            {
+                case "clear":
+                    UserInput.Clear();
+                    UserInput.Add("clear");
+                    return UserInput;
+                case "done":
+                    return UserInput;
+                case "exit":
+                    UserInput.Clear();
+                    UserInput.Add("exit");
+                    return UserInput;
+            }
+
+            return UserInput;
+        }
+
+
+
+
+        public static void Operate(Settings Config, List<string> UserInput)
+        {
+            List<string> Output = new List<string>();
+            bool topBorderToggle = false;
+            bool bottomBorderToggle = false;
+            string topBorder = null;
+            string bottomBorder = null;
+            string currentLine;
+
+            // Print borders if applicable
+            if ((Config.GetBorders() == 2) || (Config.GetBorders() == 4))
+            {
+                topBorder = BuildBorder(Config.GetBorderType(), Config.GetWidth());
+                topBorderToggle = true;
+            }
+            if ((Config.GetBorders() == 3) || (Config.GetBorders() == 4))
+            {
+                bottomBorder = BuildBorder(Config.GetBorderType(), Config.GetWidth());
+                bottomBorderToggle = true;
+            }
+
+            Console.Clear();
+
+            // Add borders to list if applicable
+            if (topBorderToggle == true)
+            {
+                currentLine = GenerateCode(topBorder, Config);
+                Output.Add(currentLine);
+            }
+
+            // FOR each list item of user input,
+            // CREATE a new list item FOR output,
+            // RUN the input THROUGH GenerateCode, and
+            // PLACE the new code INTO the new output item.
+            for (int i = 0; i < UserInput.Count; i++)
+            {
+                currentLine = GenerateCode(UserInput[i], Config);
+                Output.Add(currentLine);
+            }
+
+            if (bottomBorderToggle == true)
+            {
+                currentLine = GenerateCode(bottomBorder, Config);
+                Output.Add(currentLine);
+            }
+
+
+            // Print final results
+            for (int i = 0; i < Output.Count; i++)
+            {
+                Console.WriteLine(Output[i]);
+            }
+
+            Console.WriteLine("\r\nPress any key to continue...");
+            Console.ReadKey();
+        }
+
+
+
+
+        // Generate a line of C# code based on input from the user
+        public static string GenerateCode(string input, Settings Config)
+        {
+            string output = null;
+
+            // Apply the alignment
+            switch (Config.GetAlign())
+            {
+                case 1: // Align Left
+                    while (input.Length < Config.GetWidth())
+                    {
+                        input = string.Format("{0}{1}", input, " ");
+                    }
+                    break;
+
+                case 2: // Align Center
+                    // Set up adjust for odd string length
+                    bool oddInput = false;
+                    int adjustedWidth = Config.GetWidth();
+                    if (input.Length % 2 == 1)
+                    {
+                        adjustedWidth = Config.GetWidth() - 2;
+                        oddInput = true;
+                    }
+
+                    // Add spaces to center text
+                    while (input.Length < adjustedWidth)
+                    {
+                        input = string.Format("{0}{1}{2}", " ", input, " ");
+                    }
+
+                    // Correct for missing spaces with odd length lines
+                    if (oddInput)
+                    {
+                        input = string.Format("{0}{1}", input, " ");
+                    }
+                    break;
+
+                case 3: // Align Right
+                    while (input.Length < Config.GetWidth())
+                    {
+                        input = string.Format("{0}{1}", " ", input);
+                    }
+                    break;
+            }
+
+            input.Substring(0, 5);
+
+            // Apply the Code
+            switch (Config.GetLanguage())
+            {
+                case 1:
+                    output = string.Format("Console.WriteLine(@\"{0}\");", input);
+                    break;
+                case 2:
+                    output = string.Format("printf(\"{0}\"); **NOT FUNCTIONAL YET**", input);
+                    break;
+                case 3:
+                    output = string.Format("cout << \"{0}\"; **NOT FUNCTIONAL YET*", input);
+                    break;
+                case 4:
+                    output = string.Format("System.out.println(\"{0}\"); **NOT FUNCTIONAL YET**", input);
+                    break;
+                case 5:
+                    output = string.Format("print(\"{0}\") **NOT FUNCTIONAL YET**", input);
+                    break;
+            }
+
+            return output;
+        }
+
+
+
+
+        // Repeat the border pattern until width is surpassed, then cut it
+        public static string BuildBorder(string pattern, int width)
+        {
+            string output = "";
+
+            while (output.Length < width)
+            {
+                output = string.Format("{0}{1}", output, pattern);
+            }
+
+            output = Truncate(output, width);
+
+            return output;
+        }
+
+        // Used to cut a string down to size w/ substrings
+        public static string Truncate(string input, int maxLength)
+        {
+            if (!string.IsNullOrEmpty(input) && input.Length > maxLength)
+            {
+                return input.Substring(0, maxLength);
+            }
+
+            return input;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.Threading;
+
+namespace ASCII_to_Code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Initial declarations
+            string menuSelectionString;
+            int menuSelection;
+            Settings Config = new Settings(0, 0, null, 0, 0);
+            Config.SetAlign(1);
+            Config.SetBorders(1);
+            Config.SetLanguage(1);
+            Config.SetWidth(80);
+            Config.SetBorderType("###");
+
+            while (true)
+            {
+                PrintTitle();
+                Console.WriteLine("Welcome to the ASCII to Code generator.");
+                Console.WriteLine("What would you like to do?");
+                Console.WriteLine("\r\n1. Process ASCII text art");
+                Console.WriteLine("\r\nSETTINGS:");
+                Console.WriteLine("2. Choose program language:   [{0}]", Config.DisplayLanguage());
+                Console.WriteLine("3. Set text borders:          [{0}]", Config.DisplayBorders());
+                Console.WriteLine("4. Change border style:       [{0}]", Config.DisplayBorderType());
+                Console.WriteLine("5. Change alignment:          [{0}]", Config.DisplayAlign());
+                Console.WriteLine("6. Change character width:    [{0} characters]", Config.GetWidth());
+                Console.WriteLine("\r\n0. Exit");
+                menuSelectionString = Console.ReadLine();
+
+                if (Validate.Menu(menuSelectionString, 6))
+                {
+                    menuSelection = int.Parse(menuSelectionString);
+                }
+                else
+                {
+                    menuSelection = -1;
+                }
+
+                switch (menuSelection)
+                {
+                    case 1:
+                        Art.Create(Config);
+                        break;
+                    case 2:
+                        int language = SetLanguage();
+                        Config.SetLanguage(language);
+                        Console.ForegroundColor = ConsoleColor.DarkGreen;
+                        Console.WriteLine("Language set to {0}.", Config.DisplayLanguage());
+                        Console.ResetColor();
+                        Thread.Sleep(750);
+                        break;
+                    case 3:
+                        int borders = SetBorders();
+                        Config.SetBorders(borders);
+                        Console.ForegroundColor = ConsoleColor.DarkGreen;
+                        Console.WriteLine("Borders set to {0}.", Config.DisplayBorders());
+                        Console.ResetColor();
+                        Thread.Sleep(750);
+                        break;
+                    case 4:
+                        string border = SetBorderType();
+                        Config.SetBorderType(border);
+                        Console.ForegroundColor = ConsoleColor.DarkGreen;
+                        Console.WriteLine("Borders will now look like: {0}", Config.DisplayBorderType());
+                        Console.ResetColor();
+                        Thread.Sleep(750);
+                        break;
+                    case 5:
+                        int align = SetAlign();
+                        Config.SetAlign(align);
+                        Console.ForegroundColor = ConsoleColor.DarkGreen;
+                        Console.WriteLine("Alignment set to align {0}.", Config.DisplayAlign());
+                        Console.ResetColor();
+                        Thread.Sleep(750);
+                        break;
+                    case 6:
+                        int width = SetWidth();
+                        Config.SetWidth(width);
+                        Console.ForegroundColor = ConsoleColor.DarkGreen;
+                        Console.WriteLine("Character width set to {0} characters.", Config.GetWidth());
+                        Console.ResetColor();
+                        Thread.Sleep(750);
+                        break;
+                    case 0:
+                        Environment.Exit(0);
+                        break;
+                    case -1:
+                        break;
+                }
+            }
+        }
+
+
+
+
+        public static int SetLanguage()
+        {
+            bool inputIsBad = true;
+            int userSelection = 1;
+            string userSelectionString;
+
+            while (inputIsBad)
+            {
+                Console.Clear();
+                PrintTitle();
+                Console.WriteLine("Which language would you like to use?");
+                Console.WriteLine("1. C#");
+                Console.WriteLine("2. C");
+                Console.WriteLine("3. C++");
+                Console.WriteLine("4. Java");
+                Console.WriteLine("5. Python");
+                userSelectionString = Console.ReadLine();
+                if (!Validate.Menu(userSelectionString, 5))
+                {
+                    Console.WriteLine("Invalid entry. Please try again.");
+                } else
+                {
+                    userSelection = int.Parse(userSelectionString);
+                    inputIsBad = false;
+                }
+            }
+
+            return userSelection;
+        }
+
+
+
+
+        public static int SetBorders()
+        {
+            bool inputIsBad = true;
+            int userSelection = 1;
+            string userSelectionString;
+
+            while (inputIsBad)
+            {
+                Console.Clear();
+                PrintTitle();
+                Console.WriteLine("Which borders would you like to use?");
+                Console.WriteLine("1. None");
+                Console.WriteLine("2. Top Only");
+                Console.WriteLine("3. Bottom Only");
+                Console.WriteLine("4. Top and Bottom");
+                /*Console.WriteLine("5. Right Only");
+                Console.WriteLine("6. Left Only");
+                Console.WriteLine("7. Left and Right");
+                Console.WriteLine("8. All");*/
+                
+                userSelectionString = Console.ReadLine();
+                if (!Validate.Menu(userSelectionString, 5))
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Invalid entry. Please try again.");
+                    Console.ResetColor();
+                }
+                else
+                {
+                    userSelection = int.Parse(userSelectionString);
+                    inputIsBad = false;
+                }
+            }
+
+            return userSelection;
+        }
+
+
+
+
+        public static int SetAlign()
+        {
+            bool inputIsBad = true;
+            int userSelection = 1;
+            string userSelectionString;
+
+            while (inputIsBad)
+            {
+                Console.Clear();
+                PrintTitle();
+                Console.WriteLine("Which alignment would you like to use?");
+                Console.WriteLine("1. Left");
+                Console.WriteLine("2. Center");
+                Console.WriteLine("3. Right");
+
+                userSelectionString = Console.ReadLine();
+                if (!Validate.Menu(userSelectionString, 5))
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Invalid entry. Please try again.");
+                    Console.ResetColor();
+                }
+                else
+                {
+                    userSelection = int.Parse(userSelectionString);
+                    inputIsBad = false;
+                }
+            }
+
+            return userSelection;
+        }
+
+
+
+
+        public static int SetWidth()
+        {
+            bool inputIsBad = true;
+            int userSelection = 1;
+            string userSelectionString;
+
+            while (inputIsBad)
+            {
+                Console.Clear();
+                PrintTitle();
+                Console.WriteLine("Enter desired character width:");
+
+                userSelectionString = Console.ReadLine();
+                if (!Validate.Integer(userSelectionString))
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Invalid entry. Please try again.");
+                    Console.ResetColor();
+                }
+                else
+                {
+                    userSelection = int.Parse(userSelectionString);
+                    inputIsBad = false;
+                }
+            }
+            return userSelection;
+        }
+
+
+
+
+        public static string SetBorderType()
+        {
+            bool inputIsBad = true;
+            string userInput = null;
+
+            while (inputIsBad)
+            {
+                Console.Clear();
+                PrintTitle();
+                Console.WriteLine("Enter a one or two character border (will repeat):");
+
+                userInput = Console.ReadLine();
+                if (userInput.Length > 2)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Invalid entry. Please try again.");
+                    Console.ResetColor();
+                    Thread.Sleep(750);
+                }
+                else
+                {
+                    inputIsBad = false;
+                }
+            }
+
+            return userInput;
+        }
+
+
+
+
+        public static string Truncate(string input, int maxLength)
+        {
+            if (!string.IsNullOrEmpty(input) && input.Length > maxLength)
+            {
+                return input.Substring(0, maxLength);
+            }
+
+            return input;
+        }
+
+
+
+
+        public static void PrintTitle()
+        {
+            Console.Clear();
+            Console.WriteLine(@"################################################################################");
+            Console.WriteLine(@"                _   ___  ___ ___ ___   _          ___         _                 ");
+            Console.WriteLine(@"               /_\ / __|/ __|_ _|_ _| | |_ ___   / __|___  __| |___             ");
+            Console.WriteLine(@"              / _ \\__ \ (__ | | | |  |  _/ _ \ | (__/ _ \/ _` / -_)            ");
+            Console.WriteLine(@"             /_/ \_\___/\___|___|___|  \__\___/  \___\___/\__,_\___|            ");
+            Console.WriteLine(@"                                                                                ");
+            Console.WriteLine(@"################################################################################");
+        }
+    }
+}
